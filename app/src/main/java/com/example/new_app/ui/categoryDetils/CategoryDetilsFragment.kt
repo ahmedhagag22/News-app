995c1c5228c98066a3f61eba9484ebd308e92904
ahmedhagag22 <https://github.com/ahmedhagag22>
2023-02-21 02:03:39 +0200

    + fragment category (tab) + fragment news (show data by recyclerView ) + click on the tab ,show error ,show data on the another fragment ,call api every thing and top-headLine/sources  ) + item activity news design +rounded image by the library + load imageUrl by >glide

---
+++ app/src/main/java/com/example/new_app/ui/categoryDetils/CategoryDetilsFragment.kt
@@ -0,0 +1,136 @@
+package com.example.new_app.ui.categoryDetils
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import com.example.new_app.R
+import com.example.new_app.ApiManger
+import com.example.new_app.api.model.sourcesResponse.Source
+import com.example.new_app.api.model.sourcesResponse.SourcesResponse
+import com.example.new_app.constant
+import com.example.new_app.databinding.FragmentCategoryDetilsBinding
+import com.example.new_app.ui.news.NewsFragment
+import com.google.android.material.tabs.TabLayout
+import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class CategoryDetilsFragment : Fragment() {
+    lateinit var viewBinding: FragmentCategoryDetilsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        viewBinding = FragmentCategoryDetilsBinding.inflate(inflater, container, false)
+
+        return viewBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //load api
+        loadApi()
+
+
+    }
+
+    //show data on the frame layout >> another fragment
+    fun changeFragment(source: Source) {
+        childFragmentManager
+            .beginTransaction()
+            .replace(R.id.container_news, NewsFragment.getInstance(source))
+            .commit()
+    }
+
+    private fun loadApi() {
+        //run the progass bar and hide the text and btn
+        showLoading()
+
+        //call api
+        ApiManger
+            .getApis()
+            .getSources(constant.apiKay)
+            .enqueue(object : Callback<SourcesResponse> {
+                override fun onResponse(
+                    call: Call<SourcesResponse>,
+                    response: Response<SourcesResponse>
+                ) {
+                    if (response.isSuccessful) {
+                        viewBinding.loadingBar.isVisible = false
+
+                        //func to show sources on the tap
+                        bindSourcesInTabLayout(response.body()?.sources)
+                    } else {
+                        //convert gson (error body) to response message
+                        var gson = Gson()
+                        var errorResponse = gson
+                            .fromJson(response.errorBody()?.string(), SourcesResponse::class.java)
+
+                        //fun to show error on the txt after convert error body
+                        showErrorLayout(errorResponse.message)
+                    }
+                }
+
+                override fun onFailure(call: Call<SourcesResponse>, t: Throwable) {
+
+                    showErrorLayout(t.localizedMessage)
+
+                }
+
+            })
+
+
+    }
+
+    fun bindSourcesInTabLayout(sourcesList: List<Source?>?) {
+        // TODO: انا جاي لي ليست من نوع سورز , في نعمل عليها لوب
+        sourcesList?.forEach { source ->
+            //new tab
+            var tab = viewBinding.tabLayout.newTab()
+            tab.text = source?.name
+            tab.tag = source
+            viewBinding.tabLayout.addTab(tab)
+        }
+        viewBinding.tabLayout
+            .addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    var source = tab?.tag as Source
+                    changeFragment(source)
+
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+                }
+
+                override fun onTabReselected(tab: TabLayout.Tab?) {
+                    var source = tab?.tag as Source
+                    changeFragment(source)
+
+                }
+
+            })
+
+        //select tab num0
+        viewBinding.tabLayout.getTabAt(0)?.select()
+
+    }
+
+    fun showLoading() {
+        viewBinding.loadingBar.isVisible = true
+        viewBinding.layoutError.isVisible = false
+    }
+
+    fun showErrorLayout(message: String?) {
+        viewBinding.loadingBar.isVisible = false
+        viewBinding.layoutError.isVisible = true
+        viewBinding.txtError.text = message
+    }
+
+
+}
---
+++ app/src/main/java/com/example/new_app/ui/news/NewsAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.new_app.ui.news
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import com.bumptech.glide.Glide
+import com.example.new_app.R
+import com.example.new_app.api.model.newsResponse.News
+import com.example.new_app.databinding.NewsItemRecyclerBinding
+
+class NewsAdapter(var item:List<News?>?):RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
+
+    class ViewHolder(val viewBinding: NewsItemRecyclerBinding):RecyclerView.ViewHolder(viewBinding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            var viewBinding=NewsItemRecyclerBinding.inflate(LayoutInflater.from(parent.context)
+                ,parent,false)
+        return ViewHolder(viewBinding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.viewBinding.authorNews.text=item?.get(position)?.author
+        holder.viewBinding.title.text=item?.get(position)?.title
+        holder.viewBinding.timeNews.text=item?.get(position)?.publishedAt
+
+        //load data by the url > gilde
+        Glide.with(holder.itemView)
+            .load(item?.get(position)?.urlToImage)
+            .placeholder(R.drawable.ic_run)
+            .into(holder.viewBinding.imageNews)
+    }
+
+    override fun getItemCount(): Int {
+        return item?.size?:0
+    }
+
+    fun changeDate(articles: List<News?>?) {
+        item=articles
+        notifyDataSetChanged()
+
+    }
+}
